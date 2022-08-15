8795062ff92a8da9f8622b461dda6f7b4baa1359
saksham406 <sakshamsinha406@gmail.com>
2022-08-15 16:51:15 +0530

    Updated_cart

--- store/views.py
+++ store/views.py
@@ -1,9 +1,10 @@
-from http.client import HTTPResponse
+from django.http import HttpResponse
 from django.shortcuts import render,get_object_or_404
 from category.models import Category
 from .models import Product
 from carts.models import CartItem
 from carts.views import _cart_id
+from django.db.models import Q
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 # Create your views here.
 
@@ -50,4 +51,16 @@ def product_detail(request,category_slug,product_slug):
     return render(request,'store/product_detail.html',context)
 
 def search(request):
-    return HTTPResponse('search Page')
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count= products.count()
+        context={
+            'products':products,
+            'product_count': product_count,
+
+
+        }
+
+    return render(request,'store/store.html',context)
